00988221fedc985fa5d18cd521a5fa1b9e763cf0
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-06-12 19:05:47 +0200

    Add basic javascript

--- script.js
+++ script.js
@@ -26,26 +26,36 @@ dropdownItems.forEach(item => {
     });
 });
 
-scoreText.addEventListener('change', function(e){
-    if(player1h1>player2h1){
-      console.log('player 1 bigger');
-    }else if(player1h1<player2h1){
-      console.log('player 2 bigger');
-    }else{
-      console.log('equal');
-    }
-});
 
 p1button.addEventListener('click', function(){
   player1h1.innerText ++;
+  textColor(player1h1.innerText,player2h1.innerText);
 });
 
 p2button.addEventListener('click', function(){
   player2h1.innerText ++;
+  textColor(player1h1.innerText,player2h1.innerText);
 });
 
 resetButton.addEventListener('click', function(){
   player1h1.innerText = "0";
   player2h1.innerText = "0";
+  textColor(player1h1.innerText,player2h1.innerText);
 });
 
+function textColor(val1,val2){
+  if(parseInt(val1)>parseInt(val2)){
+    console.log('player 1 bigger');
+    player1h1.style.color = "green";
+    player2h1.style.color = "red";
+
+  }else if(parseInt(val1)<parseInt(val2)){
+    console.log('player 2 bigger');
+    player2h1.style.color = "green";
+    player1h1.style.color = "red";
+  }else{
+    console.log('equal');
+    player1h1.style.color = "black";
+    player2h1.style.color = "black";
+  }
+}
